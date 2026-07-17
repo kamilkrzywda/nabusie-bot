f5ec22f1a9e06b1808dc42e6ae80cca2c6fbe938
Kamil Krzywda <krzywda.kamil@gmail.com>
2026-07-17 17:13:33 +0000

    car repair threshold

--- main.ts
+++ main.ts
@@ -3,26 +3,27 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 
-const BASE_URL = 'https://nabusie.pl/api';
-const headers = { Cookie: `busiarz_session=${process.env.COOKIE}` };
-const maxGarageDistance = process.env.MAX_GARAGE_DISTANCE ? parseInt(process.env.MAX_GARAGE_DISTANCE) : 50; // km
-const maxJobDistance = process.env.MAX_JOB_DISTANCE ? parseInt(process.env.MAX_JOB_DISTANCE) : 50_000; // meters
-const minJobTime = process.env.MIN_JOB_TIME ? parseInt(process.env.MIN_JOB_TIME) : 300; // seconds
+export const BASE_URL = 'https://nabusie.pl/api';
+export const HEADERS = { Cookie: `busiarz_session=${process.env.COOKIE}` };
+export const MAX_GARAGE_DISTANCE = process.env.MAX_GARAGE_DISTANCE ? parseInt(process.env.MAX_GARAGE_DISTANCE) : 50; // km
+export const MAX_JOB_DISTANCE = process.env.MAX_JOB_DISTANCE ? parseInt(process.env.MAX_JOB_DISTANCE) : 50_000; // meters
+export const MIN_JOB_TIME = process.env.MIN_JOB_TIME ? parseInt(process.env.MIN_JOB_TIME) : 300; // seconds
+export const MIN_VEHICLE_CONDITION = process.env.MIN_VEHICLE_CONDITION ? parseFloat(process.env.MIN_VEHICLE_CONDITION) : 0.5; // percent
 
 class NabusieBOT {
   main = async () => {
-    const service = new NabusieService(headers, BASE_URL);
+    const service = new NabusieService(HEADERS, BASE_URL, MIN_VEHICLE_CONDITION);
     try {
       const jobs = await service.fetchJobs();
       await service.fetchState();
       for (const job of jobs.sort((a, b) => b.payout - a.payout)) {
         if (job.status !== 'OPEN') {
           console.log(`Job ${job.id} is not open. Skipping.`);
-        } else if (job.durationS < minJobTime) {
+        } else if (job.durationS < MIN_JOB_TIME) {
           console.log(`Job ${job.id} is too short (${job.durationS}s). Skipping.`);
-        } else if (job.nearestGarageKm > maxGarageDistance) {
+        } else if (job.nearestGarageKm > MAX_GARAGE_DISTANCE) {
           console.log(`Job ${job.id} is too far away (${job.nearestGarageKm}km). Skipping.`);
-        } else if (job.distanceM > maxJobDistance) {
+        } else if (job.distanceM > MAX_JOB_DISTANCE) {
           console.log(`Job ${job.id} is too long (${job.distanceM / 1000}km). Skipping.`);
         } else {
           await service.getJob(job);
--- nabusie.service.ts
+++ nabusie.service.ts
@@ -1,19 +1,16 @@
 import type { VehicleState, JobType, StateType, TripState } from './types';
+import { BASE_URL, HEADERS, MIN_VEHICLE_CONDITION } from './main';
 
 export class NabusieService {
-    private readonly baseUrl: string;
-    private readonly headers: Record<string, string>;
     private state: StateType | null = null;
 
-    constructor(headers: Record<string, string>, baseUrl: string) {
-        this.headers = headers;
-        this.baseUrl = baseUrl;
+    constructor(headers: Record<string, string>, baseUrl: string, minVehicleCondition: number) {
         return this;
     }
 
     fetchState = async (): Promise<StateType> => {
         try {
-            const res = await fetch(`${this.baseUrl}/state`, { headers: this.headers });
+            const res = await fetch(`${BASE_URL}/state`, { headers: HEADERS });
             const state = (await res.json()) as StateType;
             this.state = state;
             return state;
@@ -25,7 +22,7 @@ export class NabusieService {
 
     fetchJobs = async (): Promise<JobType[]> => {
         try {
-            const res = await fetch(`${this.baseUrl}/jobs`, { headers: this.headers });
+            const res = await fetch(`${BASE_URL}/jobs`, { headers: HEADERS });
             const jobs = (await res.json()) as { jobs: JobType[] };
             console.log(`Fetched ${jobs.jobs.length} jobs.`);
             return jobs.jobs;
@@ -37,9 +34,9 @@ export class NabusieService {
 
     takeJob = async (jobId: string, vehicleId: string) => {
         try {
-            const res = await fetch(`${this.baseUrl}/jobs/queue`, {
+            const res = await fetch(`${BASE_URL}/jobs/queue`, {
                 method: 'POST',
-                headers: this.headers,
+                headers: HEADERS,
                 body: JSON.stringify({ jobId, vehicleId, force: false }),
             });
             return res;
@@ -74,6 +71,14 @@ export class NabusieService {
         return false;
     }
 
+    removeVehicleFromState = (vehicleId: string) => {
+        if (!this.state) {
+            console.error('State is not fetched yet. Cannot remove vehicle from state.');
+            return;
+        }
+        this.state.vehicles = this.state.vehicles.filter(v => v.id !== vehicleId);
+    }
+
     getJob = async (job: JobType) => {
         await new Promise(resolve => setTimeout(resolve, 100));
         if (!this.state) {
@@ -82,7 +87,11 @@ export class NabusieService {
         }
         for (const vehicle of this.state.vehicles.sort((a, b) => a.type.capacity - b.type.capacity)) {
             if (!vehicle.type) {
-                return console.error(`Vehicle ${vehicle.id} has no type. Skipping.`);
+                console.error(`Vehicle ${vehicle.id} has no type. Skipping.`);
+            }
+            if (vehicle.condition < MIN_VEHICLE_CONDITION) {
+                this.removeVehicleFromState(vehicle.id);
+                console.log(`Vehicle ${vehicle.id} condition is too low (${vehicle.condition}). Skipping.`);
             }
             if (this.isVehicleAvailableForJob(vehicle, job)) {
                 const res = await this.takeJob(job.id, vehicle.id);
@@ -101,7 +110,7 @@ export class NabusieService {
                         console.log(`Warning - skipping job due to warning in response.`, response.warning);
                     } else {
                         console.log(`Successfully took job ${job.originName}-${job.destName} with vehicle ${vehicle.type.name} - ${vehicle.id}`);
-                        this.state.vehicles = this.state.vehicles.filter(v => v.id !== vehicle.id);
+                        this.removeVehicleFromState(vehicle.id);
                         return;
                     }
                 } else if (res && [403, 409].includes(res.status)) {
@@ -110,7 +119,6 @@ export class NabusieService {
                     return;
                 } else if (res && !res.ok) {
                     console.error(`response not ok, returning: ${res.status} ${res.statusText} ${await res.text()}`);
-                    return;
                 } else {
                     console.log('.');
                 }
